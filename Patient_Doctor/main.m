bee66e11d2a1efe39e9803a9c0257bd9a0f92899
ascoelho <anthonystefancoelho@gmail.com>
2016-05-05 11:11:04 -0400

    first commit

---
+++ Patient_Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient_Doctor
+//
+//  Created by Anthony Coelho on 2016-05-05.
+//  Copyright © 2016 Vectura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
